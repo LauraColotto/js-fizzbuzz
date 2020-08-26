ef45ef3949cc93d1c1a7d0cba4b6bc24f506473d
Lau <arinel88@gmail.com>
2020-08-26 16:28:17 +0200

    Finito esercizio

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 // Stampo i numeri da 1 a 100
 for (var i=1; i<=100; i++){
-  console.log(i);
+    if (i % 15 == 0){ // Per i multipli di 3 e 5 stampo FizzBuzz
+    console.log("FizzBuzz");
+  } else if(i % 5 == 0) // Per i multipli di 5 stampo Buzz
+    {console.log("Buzz")
+  } else if(i % 3 == 0){ // Per i multipli di 3 stampo Fizz
+    console.log("Fizz");
+  } else {
+    console.log(i);
+  }
+
 }
-// Per i multipli di 3 stampo FizzBuzz
-
-// Per i multipli di 5 stampo FizzBuzz
-
-// Per i multipli di 5 e 5 stampo FizzBuzz
