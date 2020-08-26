9ef553fca4b4dd36af0a05ed4382672dd4ffef55
Lau <arinel88@gmail.com>
2020-08-26 16:03:36 +0200

    Stampo in numeri da 1 a 100

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Stampo i numeri da 1 a 100
+for (var i=1; i<=100; i++){
+  console.log(i);
+}
+// Per i multipli di 3 stampo FizzBuzz
+
+// Per i multipli di 5 stampo FizzBuzz
+
+// Per i multipli di 5 e 5 stampo FizzBuzz
